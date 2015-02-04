42ac83ef789308e3f80ac8b10ce40d23643a72b7
Jeffrey DiMarco <jdimarco218@gmail.com>
2015-02-03 21:51:06 -0500

    Wsgi config

--- jeffDimarco/wsgi.py
+++ jeffDimarco/wsgi.py
@@ -1,8 +1,14 @@
 """
 WSGI config for jeffDimarco project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/1.7/howto/deployment/wsgi/
 """
 
-from django.core.wsgi import get_wsgi_application
-from dj_static import Cling
+import os
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "jeffDimarco.settings")
 
-application = Cling(get_wsgi_application())
+from django.core.wsgi import get_wsgi_application
+application = get_wsgi_application()
