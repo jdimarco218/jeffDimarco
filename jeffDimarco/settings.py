0849f0823e7903b6516910c14f0621c955648899
Jeffrey DiMarco <jdimarco218@gmail.com>
2015-02-03 19:43:55 -0500

    Settings

--- jeffDimarco/settings.py
+++ jeffDimarco/settings.py
@@ -93,3 +93,19 @@ STATIC_URL = '/static/'
 
 ## Jeff added
 TEMPLATE_DIRS = [os.path.join(BASE_DIR, 'templates')]
+
+# Honor the 'X-Forwarded-Proto' header for request.is_secure()
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
+# Allow all host headers
+ALLOWED_HOSTS = ['*']
+
+# Static asset configuration
+import os
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = 'staticfiles'
+STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
