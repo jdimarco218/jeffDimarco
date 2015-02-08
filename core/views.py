16289a315ee4947c39f00b8e3fd88a94008b724e
Jeffrey DiMarco <jdimarco218@gmail.com>
2015-02-08 15:15:42 -0500

    Home page init

--- core/views.py
+++ core/views.py
@@ -1,15 +1,11 @@
 # coding: utf-8
 from django.shortcuts import render_to_response
 from django.template import RequestContext
-from reviews.models import Review
-from activities.models import Activity
 from datetime import datetime
-
-def get_following_feeds(user):
 
 def home(request):
     context = RequestContext(request)
-    return render_to_response('core/cover.html', context)
+    return render_to_response('core/home.html', context)
 
 def about(request):
     context = RequestContext(request)
--- jeffDimarco/urls.py
+++ jeffDimarco/urls.py
@@ -1,7 +1,10 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+admin.autodiscover()
 
 urlpatterns = patterns('',
+    url(r'^$', 'core.views.home', name="home"),
+
     url(r'^polls/', include('polls.urls', namespace="polls")),
     url(r'^admin/', include(admin.site.urls)),
 )
