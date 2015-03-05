a13a84bc52c7e26da26b84342bab015e2cc90226
Jeffrey DiMarco <jdimarco218@gmail.com>
2015-03-04 19:24:24 -0500

    Testing notes...

--- static/web_audio.js
+++ static/web_audio.js
@@ -18,10 +18,26 @@ var buttonClickResult = function (){
     var button = document.getElementById('audio_toggle_button');
     button.onclick = function buttonClicked(){
         if(button.className=="off"){
-            window.alert("Starting oscillator!");
+            window.alert("Starting oscillator2!");
             button.className = "on";
             button.src = stopImage;
-            oscOn();
+            //oscOn();
+            var ac = new (window.AudioContext || window.webkitAudioContext);
+            // C4, E4, G4
+            var freqs = [261.63, 329.63, 392.00];
+            var oscs = [];
+            // initialize the oscillators
+            for(var i=0;i<freqs.length;i++) {
+                var o = ac.createOscillator();
+                o.frequency.value = freqs[i];
+                o.connect(ac.destination);
+                oscs.push(o);
+            }
+            // schedule noteOn and noteOff (deprecated: the methods will be renamed to start() and   stop() soon)
+            for (i = 0; i < oscs.length; i +=1) {
+                oscs[i].noteOn(0);
+                oscs[i].noteOff(1);
+            }
         } else if(button.className == "on"){
             window.alert("Stopping oscillator!");
             button.className = "off";
